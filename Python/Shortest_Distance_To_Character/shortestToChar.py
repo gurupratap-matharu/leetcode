6fa083c67d86e6b26cc0c3d0328b5c344e05629a
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2018-08-19 19:32:23 -0300

    Shortest Distance is working now. First draft ready. Now Unit testing.

---
+++ Python/Shortest_Distance_To_Character/shortestToChar.py
@@ -0,0 +1,33 @@
+class Solution:
+    def shortestToChar(self, S, C):
+        """
+        :type S: str
+        :type C: str
+        :rtype: List[int]
+        """
+        self.string = list(S)
+        self.char = C
+
+        lst = [abs(x-self.string.index(self.char)) for x in range(len(self.string))]
+
+        while True:
+            try:
+
+                distance = self.string.index(self.char)
+
+                for i in range(len(self.string)):
+                    if abs(i-distance) < lst[i]:
+                        lst[i] = abs(i-distance)
+
+                self.string[distance] = '#'
+
+            except ValueError:
+
+                break
+            print(lst)
+            return lst
+sol = Solution()
+sol.shortestToChar('inderpalsinghmatharu', 'e')
+
+# print(sol.string)
+# print(sol.char)
